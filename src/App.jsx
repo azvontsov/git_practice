96d4e30032b122588427e0826e5c9cf984950e79
Anton Zvontcov <azvontcov@asginc.us>
2023-06-28 14:39:51 -0400

    added margins

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,12 @@ function App() {
       <h1>Git rebase practice</h1>
       <div className="card">
         <p>Adds new button</p>
-        <button style={{ backgroundColor: 'gold' }}>New Button</button>
-        <button style={{ backgroundColor: 'red' }}>Adds Button in GitHub</button>
-        
+        <button style={{ backgroundColor: 'gold', marginRight: '5px' }}>
+          New Button
+        </button>
+        <button style={{ backgroundColor: 'red' }}>
+          Adds Button in GitHub
+        </button>
       </div>
     </>
   );
