daf86cc1b7ed5e2a3f49601df0b4ff649822a5b7
Anton Zvontcov <azvontcov@asginc.us>
2023-06-28 13:30:44 -0400

    initial state

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Git rebase practice</h1>
+      <div className="card">
+        <p>Sample text</p>
+      </div>
+      K
+    </>
+  );
+}
+
+export default App;
